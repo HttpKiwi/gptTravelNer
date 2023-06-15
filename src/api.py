be0fe8580c1b01677613bea51f39de5412e41d68
Juan Pablo Dominguez <juan.avenia@correounivalle.edu.co>
2023-06-15 11:46:22 -0500

    Use new url for Creta

--- src/api.py
+++ src/api.py
@@ -2,5 +2,5 @@ import requests
 
 
 def minizinc_api(object):
-    res = requests.get("http://143.198.177.199:3666/all", json=object)
+    res = requests.get("http://143.198.177.199/all", json=object)
     return res.json()
